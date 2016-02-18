182af4226333e6004412e71270f343ceedc34c85
Richard Meadows <richardeoin@gmail.com>
2016-02-18 18:34:24 +0000

    [wspr testvectors] Passing testvectors for WSPR

--- tests/wspr.rs
+++ tests/wspr.rs
@@ -46,3 +46,55 @@ fn wspr_locator_should_panic() {
 fn wspr() {
     let _ = wspr::encode_wspr("M0AAA", "AA00", 30);
 }
+
+
+///
+/// WSPR
+///
+mod testvectors;
+#[test]
+fn wspr_testvectors() {
+    let n_symbols = 162;
+    let mut testvectors = testvectors::parse_wspr_testvectors("wspr", 162);
+
+    let mut it_went_wrong = false;
+
+    while let Some((message, vectors)) = testvectors.pop() { // for each testvector
+        let mut msplit = message.split_whitespace();
+
+        match (msplit.next(), msplit.next(), msplit.next()) { // Attempt to parse as three parts
+            (Some(callsign), Some(locator), Some(p)) => {
+
+                if let Ok(power) = p.parse::<i32>() { // Attempt to parse power
+                    let test_result: [u8; 162] = wspr::encode_wspr(&callsign, &locator, power); // encode it ourselves
+
+                    let mut d_symbols: u32 = 0;
+
+                    for i in 0..n_symbols {
+                        if test_result[i] != vectors[i] { // symbol doesn't match!
+                            println!("index {}: test {} -- reference {}", i, test_result[i], vectors[i]);
+                            d_symbols += 1;
+                        }
+                    }
+
+                    if d_symbols > 0 {
+                        println!("");
+                        println!("Message failed to pass: {}", message);
+                        println!("(Total symbol errors {}/{})", d_symbols, n_symbols);
+                        it_went_wrong = true;
+                    }
+                } else {
+                    panic!("Supplied power \"{}\" could not be parsed as i32", p);
+                }
+            },
+            (_, _, _) => {
+                panic!("Message \"{}\" supplied by testvector is not valid!", message);
+            }
+        }
+    };
+
+    if it_went_wrong {
+        println!("");
+        panic!("See failures above");
+    }
+}
